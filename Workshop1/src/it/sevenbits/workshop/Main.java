2d7b6996680fa0e67e9099b09f684525c863e7cb
smailik120 <stasras2000@mail.ru>
2018-10-12 18:59:36 +0600

    first commit

---
+++ Workshop1/src/it/sevenbits/workshop/Main.java
@@ -0,0 +1,11 @@
+package it.sevenbits.workshop;
+
+import it.sevenbits.workshop.matrix.Matrix;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Matrix matrix = new Matrix(3,4);
+        System.out.print(matrix.toString());
+    }
+}
